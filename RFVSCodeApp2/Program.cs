4bc9fe1238382af7b688494e08d58533f5c15baa
richfrem <36930640+richfrem@users.noreply.github.com>
2024-06-02 13:01:53 -0700

    Update Program.cs

--- RFVSCodeApp2/Program.cs
+++ RFVSCodeApp2/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!  This is my 2nd .NET app from VS Code.  Edited from within GitHub.  ");
+app.MapGet("/", () => "Hello World!  This is my 2nd .NET app from VS Code.  Edited from within GitHub2.  ");
 
 app.Run();
